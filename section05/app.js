53e668ed7ab8e1e74d2087fc491de8bfec5900ae
kim <yuyukgh@naver.com>
2026-03-29 14:42:50 +0900

    add section05 with pug

--- section05/app.js
+++ section05/app.js
@@ -4,10 +4,12 @@ const bodyParser = require("body-parser");
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const app = express();
+app.set("view engine", "pug");
+app.set("views", "pug");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
-app.use("/admin", adminRoutes);
+app.use("/admin", adminRoutes.router);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
--- section05/routes/admin.js
+++ section05/routes/admin.js
@@ -3,13 +3,17 @@ const path = require("path");
 
 const router = express.Router();
 
+const products = [];
+
 router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
+  // res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
+  res.render("add-product", { jobTitle: "Add Product" });
 });
 
 router.post("/products", (req, res, next) => {
-  console.log(req.body);
+  products.push({ title: req.body.title });
   res.redirect("/");
 });
 
-module.exports = router;
+exports.router = router;
+exports.products = products;
